fb893e7e3a9db3542f572c1fac5bfb136fd5dbde
007 <132037908+khadherinc@users.noreply.github.com>
2023-11-23 07:48:15 -0800

    test

--- commands/deepsea.js
+++ commands/deepsea.js
@@ -16,15 +16,15 @@
  const maker = require('mumaker')
  
      //---------------------------------------------------------------------------
- cmd({ pattern: "deepsea", category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
+ cmd({ pattern: "thunder", category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
          if (!text) return citel.reply('_Need text._')
-         let anu = await maker.textpro('https://textpro.me/create-3d-deep-sea-metal-text-effect-online-1053.html', text)
+         let anu = await maker.textpro('https://yx-api.herokuapp.com/api/textpro/thunder?text=${text})
          Void.sendMessage(citel.chat, { image: { url: anu }, caption: `Made by ${tlang().title},For my ${tlang().greet}` }, { quoted: citel })
      })
      //---------------------------------------------------------------------------
- cmd({ pattern: "horror", category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
+ cmd({ pattern: "breakwall", category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
          if (!text) return citel.reply('_Need text._')
-         let anu = await maker.textpro('https://textpro.me/horror-blood-text-effect-online-883.html', text)
+         let anu = await maker.textpro('https://yx-api.herokuapp.com/api/textpro/breakwall?text=${text})
          Void.sendMessage(citel.chat, { image: { url: anu }, caption: `Made by ${tlang().title},For my ${tlang().greet}` }, { quoted: citel })
      })
      //---------------------------------------------------------------------------
